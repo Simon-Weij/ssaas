3faf7777151b54d3af09cd681ef10fb22e6bd102
Simon <175155691+Simon-Weij@users.noreply.github.com>
2025-12-24 21:39:14 +0100

    Added page switching and vm

---
+++ frontend/src/components/card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Arrow from "./icons/arrow.svelte";
+	import { goto } from '$app/navigation';
+
+  export let image;
+  export let alt;
+  export let title;
+  export let paragraph;
+</script>
+
+<div class="card bg-base-100 w-96 shadow-sm border border-base-300">
+  
+  <figure>
+    <img
+      src={image}
+      alt={alt}
+      class="w-48 h-auto mx-auto primary" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">{title}</h2>
+    <p>{paragraph}</p>
+    <div class="card-actions justify-end">
+      <button class="btn" on:click={() => goto('/vm')}><Arrow /></button>
+    </div>
+  </div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Card from "../components/card.svelte";
+	import vmImage from '$lib/assets/vm.png';
+  
+</script>
+<main class="container mx-auto flex flex-wrap justify-center gap-2 px-8 py-4">
+  <Card 
+    image={vmImage}
+    alt="Virtual machine"
+    title="Virtual machines"
+    paragraph="Manage virtual machines"
+    />
+  <Card 
+    image={vmImage}
+    alt="Virtual machine"
+    title="Virtual machines"
+    paragraph="Manage virtual machines"
+    />
+  <Card 
+    image={vmImage}
+    alt="Virtual machine"
+    title="Virtual machines"
+    paragraph="Manage virtual machines"
+    />
+  <Card 
+    image={vmImage}
+    alt="Virtual machine"
+    title="Virtual machines"
+    paragraph="Manage virtual machines"
+    />
+  <Card 
+    image={vmImage}
+    alt="Virtual machine"
+    title="Virtual machines"
+    paragraph="Manage virtual machines"
+    />
+  <Card 
+    image={vmImage}
+    alt="Virtual machine"
+    title="Virtual machines"
+    paragraph="Manage virtual machines"
+    />
+</main>
